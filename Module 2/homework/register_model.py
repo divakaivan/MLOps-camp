8607ff29501ba51e6a4e999a091b14734e831e95
divakaivan <timee98@outlook.com>
2024-05-26 14:25:18 +0900

    better loading of params in top 5 models

--- Module 2/homework/register_model.py
+++ Module 2/homework/register_model.py
@@ -12,7 +12,7 @@ HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
 EXPERIMENT_NAME = "random-forest-best-models"
 RF_PARAMS = ['max_depth', 'n_estimators', 'min_samples_split', 'min_samples_leaf', 'random_state']
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://127.0.0.1:8080")
 mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
@@ -28,10 +28,11 @@ def train_and_log_model(data_path, params):
     X_test, y_test = load_pickle(os.path.join(data_path, "test.pkl"))
 
     with mlflow.start_run():
+        new_params = {}
         for param in RF_PARAMS:
-            params[param] = int(params[param])
+            new_params[param] = int(params[param])
 
-        rf = RandomForestRegressor(**params)
+        rf = RandomForestRegressor(**new_params)
         rf.fit(X_train, y_train)
 
         # Evaluate model on the validation and test sets
@@ -66,50 +67,6 @@ def run_register_model(data_path: str, top_n: int):
         order_by=["metrics.rmse ASC"]
     )
     for run in runs:
-        param_types = {
-        'bootstrap': bool,
-        'ccp_alpha': float,
-        'criterion': str,
-        'max_depth': int,
-        'max_features': float,
-        'max_leaf_nodes': int,
-        'max_samples': float,
-        'min_impurity_decrease': float,
-        'min_samples_leaf': int,
-        'min_samples_split': int,
-        'min_weight_fraction_leaf': float,
-        'monotonic_cst': None,
-        'n_estimators': int,
-        'n_jobs': int,
-        'oob_score': bool,
-        'random_state': int,
-        'verbose': int,
-        'warm_start': bool
-    }
-        params = run.data.params
-    # Convert params to their appropriate types
-        for param, param_type in param_types.items():
-            if param in params:
-                value = params[param]
-                if param_type == bool:
-                    params[param] = value == 'True'
-                elif param_type == int:
-                    if value.lower() == 'none':
-                        params[param] = None
-                    else:
-                        params[param] = int(value)
-                elif param_type == float:
-                    if value.lower() == 'none':
-                        params[param] = None
-                    else:
-                        params[param] = float(value)
-                elif param_type == str:
-                    params[param] = value
-                elif param_type is None:
-                    params[param] = None  # For parameters that can be None
-                else:
-                    raise ValueError(f"Unsupported parameter type: {param_type} for parameter: {param}")
-
         train_and_log_model(data_path=data_path, params=run.data.params)
 
     # Select the model with the lowest test RMSE
